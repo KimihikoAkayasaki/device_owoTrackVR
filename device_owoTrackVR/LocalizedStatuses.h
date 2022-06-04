57c8aeab8752bcf5e552089f8b3f6cbd8074af26
公彦赤屋先 <kimihikoakaya@gmail.com>
2022-06-04 18:37:40 +0900

    Update OWOVR's localization resources [^Ella]

--- device_owoTrackVR/LocalizedStatuses.h
+++ device_owoTrackVR/LocalizedStatuses.h
@@ -14,6 +14,10 @@ inline std::map<std::wstring, std::wstring> status_ok_map
 	{
 		L"lc-LC",
 		L"Shucshesh!\nS_OK\nEvewytshin's gud!"
+	},
+	{
+		L"fr-FR",
+		L"Succès!\nS_OK\nTout fonctionne!"
 	}
 };
 
@@ -26,6 +30,10 @@ inline std::map<std::wstring, std::wstring> status_dead_map
 	{
 		L"lc-LC",
 		L"Connecshion ewwow!\nE_CONNECTION_DEAD\nCshechk f owoTrackVR ap s wunninwg, wowkinwg n connecshted."
+	},
+	{
+		L"fr-FR",
+		L"Erreur de connexion!\nE_CONNECTION_DEAD\nVérifiez si l'app owoTrackVR fonctionne et est connectée."
 	}
 };
 
@@ -38,6 +46,10 @@ inline std::map<std::wstring, std::wstring> status_no_data_map
 	{
 		L"lc-LC",
 		L"Connecshion ewwow!\nE_NO_DATA\nCshechk f owoTrackVR ap s wunninwg, wowkinwg n connecshted."
+	},
+	{
+		L"fr-FR",
+		L"Erreur de connexion!\nE_NO_DATA\nVérifiez si l'app owoTrackVR fonctionne et est connectée."
 	}
 };
 
@@ -50,6 +62,10 @@ inline std::map<std::wstring, std::wstring> status_init_fail_map
 	{
 		L"lc-LC",
 		L"Listwenew shtawtup faiw!\nE_INIT_FAILED\nDe dat sewvew faiwd to shtawt, cshechk logws n powt nuwmbew."
+	},
+	{
+		L"fr-FR",
+		L"Erreur du serveur!\nE_INIT_FAILED\nLe serveur n'a pas démarré correctement. Vérifiez les logs et le port."
 	}
 };
 
@@ -57,11 +73,15 @@ inline std::map<std::wstring, std::wstring> status_not_started_map
 {
 	{
 		L"en-US",
-		L"Connection error!\nE_NOT_STARTED\nPress the 'Reconnect' button to start the server's listener up."
+		L"Connection error!\nE_NOT_STARTED\nPress the 'Refresh' button to start the server's listener up."
 	},
 	{
 		L"lc-LC",
 		L"Connecshion ewwow!\nE_NOT_STARTED\nPwess de 'Weconnect' wuwwow t stawt de sewvew's listewnew up."
+	},
+	{
+		L"fr-FR",
+		L"Erreur de connexion!\nE_NOT_STARTED\nCliquez sur 'Actualiser' pour démarrer le serveur owoTrack."
 	}
 };
 
