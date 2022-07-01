1da23359004a4cc1aaf62faef757a276058dd0de
公彦赤屋先 <kimihikoakaya@gmail.com>
2022-07-02 05:07:46 +0900

    Added de-DE resources

--- device_owoTrackVR/LocalizedSettings.h
+++ device_owoTrackVR/LocalizedSettings.h
@@ -14,6 +14,10 @@ inline std::map<std::wstring, std::wstring> local_ip_single_label_map
 	{
 		L"lc-LC",
 		L"Your Local IP: "
+	},
+	{
+		L"de-DE",
+		L"Deine Lokale IP: "
 	}
 };
 
@@ -26,6 +30,10 @@ inline std::map<std::wstring, std::wstring> local_ip_multiple_label_map
 	{
 		L"lc-LC",
 		L"Your Local IP: (One of) "
+	},
+	{
+		L"de-DE",
+		L"Einer deiner lokalen IPs: "
 	}
 };
 
@@ -39,6 +47,10 @@ inline std::map<std::wstring, std::wstring> connection_port_label_map
 	{
 		L"lc-LC",
 		L"Connection Port: \n"
+	},
+	{
+		L"de-DE",
+		L"Verbindungsport: \n"
 	}
 };
 
@@ -52,6 +64,10 @@ inline std::map<std::wstring, std::wstring> notice_not_started_map
 	{
 		L"lc-LC",
 		L"Please start the server first!"
+	},
+	{
+		L"de-DE",
+		L"Starte den Server bitte zuerst!"
 	}
 };
 
@@ -65,6 +81,10 @@ inline std::map<std::wstring, std::wstring> notice_not_connected_map
 	{
 		L"lc-LC",
 		L"Please connect your phone!"
+	},
+	{
+		L"de-DE",
+		L"Verbinde dich mit deinem Telefon!"
 	}
 };
 
@@ -78,6 +98,10 @@ inline std::map<std::wstring, std::wstring> button_calibrate_forward_map
 	{
 		L"lc-LC",
 		L"Calibrate Forward"
+	},
+	{
+		L"de-DE",
+		L"Vorwärts kalibrieren"
 	}
 };
 
@@ -91,6 +115,10 @@ inline std::map<std::wstring, std::wstring> button_calibrate_down_map
 	{
 		L"lc-LC",
 		L"Calibrate Down"
+	},
+	{
+		L"de-DE",
+		L"Rückwärts kalibrieren"
 	}
 };
 
@@ -104,6 +132,10 @@ inline std::map<std::wstring, std::wstring> calibration_instructions_forward_map
 	{
 		L"lc-LC",
 		L"Hold your phone in the same direction\nas your VR headset orientation, screen facing up..."
+	},
+	{
+		L"de-DE",
+		L"Halte dein Telefon in die gleiche Richtung\nwie dein VR-Headset. Bildschirm sollte nach oben zeigen."
 	}
 };
 
@@ -117,6 +149,10 @@ inline std::map<std::wstring, std::wstring> calibration_instructions_down_map
 	{
 		L"lc-LC",
 		L"Hold your phone in the same direction\nas your VR headset orientation, screen facing up..."
+	},
+	{
+		L"de-DE",
+		L"Halte dein Telefon in die gleiche Richtung\nwie dein VR-Headset. Bildschirm sollte nach oben zeigen."
 	}
 };
 
@@ -130,6 +166,10 @@ inline std::map<std::wstring, std::wstring> stay_still_map
 	{
 		L"lc-LC",
 		L"Please stay like that a bit..."
+	},
+	{
+		L"de-DE",
+		L"Bitte bleib noch ein bisschen so..."
 	}
 };
 
@@ -144,5 +184,9 @@ inline std::map<std::wstring, std::wstring> server_failure_map
 	{
 		L"lc-LC",
 		L"Server has failed to start up!"
+	},
+	{
+		L"de-DE",
+		L"Server konnte nicht gestartet werden!"
 	}
 };
--- device_owoTrackVR/LocalizedStatuses.h
+++ device_owoTrackVR/LocalizedStatuses.h
@@ -22,6 +22,10 @@ inline std::map<std::wstring, std::wstring> status_ok_map
 	{
 		L"ru-RU",
 		L"Успешно!\nS_OK\nВсе работает!"
+	},
+	{
+		L"de-DE",
+		L"Erfolg!\nS_OK\nAlles ist gut!"
 	}
 };
 
@@ -42,6 +46,10 @@ inline std::map<std::wstring, std::wstring> status_dead_map
 	{
 		L"ru-RU",
 		L"Ошибка подключения!\nE_CONNECTION_DEAD\nУбедитесь, что приложение owoTrackVR работает и подключено."
+	},
+	{
+		L"de-DE",
+		L"Verbindungsfehler!\nE_CONNECTION_DEAD\nÜberprüfe ob die owoTrackVR-App läuft, funktioniert und verbunden ist."
 	}
 };
 
@@ -62,6 +70,10 @@ inline std::map<std::wstring, std::wstring> status_no_data_map
 	{
 		L"ru-RU",
 		L"Ошибка подключения!\nE_NO_DATA\nУбедитесь, что приложение owoTrackVR работает и подключено."
+	},
+	{
+		L"de-DE",
+		L"Verbindungsfehler!\nE_NO_DATA\nÜberprüfe ob die owoTrackVR-App läuft, funktioniert und verbunden ist."
 	}
 };
 
@@ -82,6 +94,10 @@ inline std::map<std::wstring, std::wstring> status_init_fail_map
 	{
 		L"ru-RU",
 		L"Ошибка запуска сервера!\nE_INIT_FAILED\nНе удалось запустить сервер, проверьте журнал событий и номер порта."
+	},
+	{
+		L"de-DE",
+		L"Serverfehler!\nE_INIT_FAILED\nDer Datenserver konnte nicht gestartet werden. Überprüfe die Logs und die Portnummer."
 	}
 };
 
@@ -102,6 +118,10 @@ inline std::map<std::wstring, std::wstring> status_not_started_map
 	{
 		L"ru-RU",
 		L"Ошибка подключения!\nE_NOT_STARTED\nНажмите кнопку \"Обновить\" для запуска сервера."
+	},
+	{
+		L"de-DE",
+		L"Verbindungsfehler!\nE_NOT_STARTED\nKlicke auf „Aktualisieren“ um den Listener des Servers zu starten."
 	}
 };
 
@@ -110,11 +130,11 @@ inline std::map<std::wstring, std::wstring> status_not_started_map
 // Get the current use language, e.g. en-US
 inline std::wstring GetUserLocale()
 {
-	wchar_t localeName[LOCALE_NAME_MAX_LENGTH] = { 0 };
+	wchar_t localeName[LOCALE_NAME_MAX_LENGTH] = {0};
 	return GetUserDefaultLocaleName(
-		localeName, sizeof(localeName) / sizeof(*(localeName))) == 0
-		? std::wstring()
-		: localeName;
+		       localeName, sizeof(localeName) / sizeof(*(localeName))) == 0
+		       ? std::wstring()
+		       : localeName;
 }
 
 // Get the current status string (but localized)
@@ -122,8 +142,8 @@ inline std::wstring GetLocalizedStatusWString(
 	std::wstring locale, std::map<std::wstring, std::wstring> status_map)
 {
 	return status_map.contains(locale)
-		? status_map[locale]
-		: status_map[L"en-US"];
+		       ? status_map[locale]
+		       : status_map[L"en-US"];
 }
 
 // Get the current status string (but localized)
@@ -131,6 +151,6 @@ inline std::wstring GetLocalizedStatusWStringAutomatic(
 	std::map<std::wstring, std::wstring> status_map)
 {
 	return status_map.contains(GetUserLocale())
-		? status_map[GetUserLocale()]
-		: status_map[L"en-US"];
+		       ? status_map[GetUserLocale()]
+		       : status_map[L"en-US"];
 }
